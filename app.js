b53ec3a19bbd3d3eab6a0c6c30ca6196ae9ccf5d
shouko <imshouko@gmail.com>
2020-03-29 23:12:32 +0900

    Add mongoose

--- app.js
+++ app.js
@@ -2,9 +2,36 @@ require('dotenv').config();
 const express = require('express');
 const bunyan = require('bunyan');
 const http = require('http');
+const mongoose = require('mongoose');
+
+const logger = bunyan.createLogger({ name: 'kurumi-base' });
+
+let dbConnected = false;
+const dbConnect = () => {
+  mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+};
+dbConnect();
+
+mongoose.connection.on('error', () => {
+  logger.error('MongoDB connection error.');
+});
+mongoose.connection.on('disconnected', () => {
+  logger.error('MongoDB disconnected');
+  if (!dbConnected) dbConnect();
+});
+mongoose.connection.on('connected', () => {
+  dbConnected = true;
+  logger.info('MongoDB connection established');
+});
+mongoose.connection.on('reconnected', () => {
+  logger.info('MongoDB reconnected.');
+});
 
 const app = express();
-app.locals.logger = bunyan.createLogger({ name: 'kurumi-base' });
+app.locals.logger = logger;
 app.locals.httpAgent = new http.Agent();
 app.locals.httpAgent.maxSockets = 10;
 
@@ -13,4 +40,11 @@ app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/dcimg/:id', require('./routes/dcimg'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => app.locals.logger.info(`Listening on port ${port}!`));
+app.listen(port, () => logger.info(`Listening on port ${port}!`));
+
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    logger.info('Closing MongoDB connection.');
+    process.exit(0);
+  });
+});
