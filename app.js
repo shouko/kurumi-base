bc5e423fe4b56ad6e066b397569b7467648568f7
shouko <imshouko@gmail.com>
2020-03-31 00:25:37 +0900

    Add body-parser

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bunyan = require('bunyan');
 const http = require('http');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const config = require('./config');
 
 const logger = bunyan.createLogger({ name: 'kurumi-base' });
@@ -35,6 +36,9 @@ app.locals.logger = logger;
 app.locals.httpAgent = new http.Agent();
 app.locals.httpAgent.maxSockets = 10;
 app.locals.storage = require('./utils/storage');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
