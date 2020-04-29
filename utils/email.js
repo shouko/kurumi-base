40c61d019b1af801d2e00066b010fbdfd11fb84c
shouko <imshouko@gmail.com>
2020-04-30 01:02:49 +0900

    Add unit test

--- utils/email.js
+++ utils/email.js
@@ -1,9 +1,9 @@
 module.exports = {
   parseFrom: (input) => {
-    const rgx = /^([^<]+ )?<?(([A-z0-9_-]+)@([^>]+))>?$/;
+    const rgx = /^(([^<]+) )?<?(([A-z0-9_-]+)@([^>]+))>?$/;
     const matches = rgx.exec(input);
     if (!matches) return false;
-    const [, name, address, username, domain] = matches;
+    const [, , name, address, username, domain] = matches;
     return {
       name, address, username, domain,
     };
