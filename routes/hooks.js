81fd6ca699c9f286cf57a578de1c2758df1a2e52
shouko <imshouko@gmail.com>
2020-04-29 15:56:41 +0900

    Print req body at mail webhook

--- routes/hooks.js
+++ routes/hooks.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const multer = require('multer')();
 const Mail = require('../models/Mail');
 const auth = require('../middleware/auth');
+const logger = require('../services/logger');
 
 router.get('/mail', (req, res) => res.sendStatus(405));
 
@@ -10,6 +11,7 @@ router.post('/mail', auth.sendgridIncoming, multer.any(), (req, res) => {
     payload: req.body,
   }).save((err) => {
     if (err) return res.sendStatus(500);
+    logger.info(req.body);
     return res.sendStatus(200);
   });
 });
